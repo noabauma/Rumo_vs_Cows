c0edcbdaf464368b8387da39901146301d57c774
Noah Baumann <noabauma@student.ethz.ch>
2025-06-10 20:49:17 +0200

    moving the animation script into the discrete folder as very folder gets its own animation

---
+++ discrete/anim.py
@@ -0,0 +1,101 @@
+from manim import *
+import numpy as np
+
+from main import * # all the functions of discrete/main.py
+
+np.random.seed(42)   # seed for the random number generator
+
+class Discrete(MovingCameraScene):        
+    def construct(self):
+        time_start = time.time()
+    
+        ##### Step 1: Let's build the problem field
+        x_length = 50        # x coordinate of the cows field [m]
+        y_length = 50        # y coordinate of the cows field [m]
+        n_obst = 10          # number of obsticles (cows)
+        
+        np.random.seed(42)   # seed for the random number generator
+            
+        grid_spacing = 1        # spacing of the grid points [m]
+        
+        obst_coord = np.random.rand(n_obst, 2)
+        
+        obst_coord[:,0] *= x_length
+        obst_coord[:,1] *= y_length
+
+        ##### Step 2: Computing the 2d heatmap of the cows in the field
+        grid_points, n_total_points, n_grid_points_x = compute_heatmap(obst_coord, x_length, y_length, grid_spacing)
+        
+        ##### Step 3: Converting the heatmap into a weighted graph
+        graph = compute_graph(grid_points, n_total_points, n_grid_points_x)
+        
+        # Define the starting and end points (as indices in the graph)    
+        start_coord = int(np.random.random_sample()*x_length + 0.5)                                                         # 0
+        end_coord = grid_points.shape[0] - int(x_length/grid_spacing + 1) + int(np.random.random_sample()*x_length + 0.5)   # -1
+        
+        # Swap the end point with the current last one
+        swap_nodes(graph, end_coord, -1)
+        
+        ##### Step 4: Compute the shortest path
+        dist_matrix, predecessors = shortest_path(csgraph=graph, method='auto', directed=False, indices=start_coord, return_predecessors=True)
+        
+        # Backtrack to find the shortest path from source to destination
+        path = []
+        step = end_coord
+        while step != start_coord:
+            path.append(step)
+            step = predecessors[step]
+
+        path.append(start_coord)
+        path = path[::-1]  # Reverse the path to get it from source to destination
+
+        ##### Step 5 manim the shit out of it!
+        
+        # At the border (i.e. Rectangle)
+        rect = Rectangle(width=x_length, height=y_length).move_to(x_length/2*RIGHT+y_length/2*UP)
+        
+        # Move the camera to the rectangle
+        margin = max(x_length, y_length)*0.1
+        self.play(self.camera.frame.animate.move_to(rect).set(width=x_length + margin, height=y_length + margin))
+        
+        # Save the state of camera
+        self.camera.frame.save_state()
+
+        # Create Dot mobjects from the array
+        points = VGroup(*[
+            Dot(point=[x, y, 0], radius=0.5, color=LIGHT_BROWN)
+            for x, y in obst_coord
+        ])
+        
+        # Make the cows infront of the grid_points
+        points.set_z_index(1)
+        
+        grid_points_ = VGroup(*[
+            Dot(point=[x, y, 0], radius=0.2, color=RED)
+            for x, y, z in grid_points
+        ])
+        
+        grid_points_.set_z_index(0)
+        
+        # Labels for z-values (heatmap values)
+        labels = VGroup(*[
+            DecimalNumber(z, num_decimal_places=2, color=WHITE)
+            .scale(0.4)
+            .next_to([x, y, 0], UP, buff=0.1)
+            for x, y, z in grid_points
+        ])
+        
+        labels.set_z_index(1)
+
+        # Animate
+        self.play(Create(rect))
+        self.play(LaggedStartMap(FadeIn, points, lag_ratio=0.05))
+        self.play(Create(grid_points_))
+        self.wait()
+        
+        # Let's move the camera to a corner
+        margin = max(x_length, y_length)/100  # optional margin
+        self.play(self.camera.frame.animate.move_to([x_length/20, y_length*(19/20), 0]).set(width=x_length/10 + margin , height=y_length/10 + margin ))
+        self.wait()
+        self.play(Create(labels))
+
